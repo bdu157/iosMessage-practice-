4becf0f251a221f51ba3f7f7c3e88c651f4a4be2
Dongwoo Pae <dongwoopae@gmail.com>
2019-05-31 23:19:32 -0700

    first commit

---
+++ iosMessage(practice)/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iosMessage(practice)
+//
+//  Created by Dongwoo Pae on 5/31/19.
+//  Copyright © 2019 Dongwoo Pae. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
